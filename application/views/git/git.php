bf1244172c4666bebf7c861c38e973b2d643f26b
Malinao <Malinao.com>
2019-03-24 22:30:59 +0800

    Sample Message

---
+++ application/views/git/git.php
@@ -0,0 +1,8 @@
+<h1>
+	Hello World
+</h1>
+
+	<li> Users Change</li>
+	<li> </li>
+	<li> </li>
+	</ul>
---
+++ application/views/templates/footer.php
@@ -0,0 +1,11 @@
+    
+
+
+	<footer>
+		<p align="center">Coptyright &copy; 2018 | All Rights Reserved</p>
+	</footer>
+    <script src="<?php  echo base_url("assets/js/test.js"); ?>"></script>
+
+   
+  </body>
+</html>
